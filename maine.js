280d03381b893d16257505feac7b2327347af47f
nino-18 <132003814+nino-18@users.noreply.github.com>
2023-05-12 17:13:02 +0400

    first commit

---
+++ maine.js
@@ -0,0 +1,60 @@
+// let age=prompt('Please enter you age');
+
+// if(age>=18){
+//     let name =prompt('Please enter your name');
+//     let lastname=prompt("please enter you lastname" );
+//     console.log('hello'+name+lastname);
+// }else{
+//     console.log('Sorry you are not adult');
+// }
+
+
+// let correctAnswers = 0;
+
+// let response1 = prompt("რომელი საქართველოს დედაქალაქი");
+// if (response1.toLowerCase() === "თბილისი") {
+//   correctAnswers++;
+// }
+
+// let response2 = prompt("რომელია ყველაზე დიდი პლანეტა მზის სისტემაში");
+// if (response2.toLowerCase() === "იუპიტერი") {
+//   correctAnswers++;
+// }
+
+// let response3 = prompt("რომელია ყველაზე დიდი ცხოველი?");
+// if (response3.toLowerCase() === "სპილო") {
+//   correctAnswers++;
+// }
+
+// console.log(`You answered ${correctAnswers} out of 3 questions correctly.`);
+
+
+// let num1 = parseFloat(prompt("Enter the first number:"));
+// let num2 = parseFloat(prompt("Enter the second number:"));
+// let operator = prompt("Select an operator (+, -, *, /):");
+
+// let result;
+
+// switch(operator) {
+//   case "+":
+//     result = num1 + num2;
+//     break;
+//   case "-":
+//     result = num1 - num2;
+//     break;
+//   case "*":
+//     result = num1 * num2;
+//     break;
+//   case "/":
+//     result = num1 / num2;
+//     break;
+//   default:
+//     console.log("Invalid operator selected.");
+//     break;
+// }
+
+// if (operator === "=") {
+//   console.log(`The result is: ${num1}`);
+// } else if (result !== undefined) {
+//   console.log(`The result is: ${result}`);
+// }
